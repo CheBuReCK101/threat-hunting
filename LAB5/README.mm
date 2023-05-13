a1d24fb2cc1c1ade9172b510fb4cabe6a997462c
CheBuReCK101 <dutovalexandr2002@yandex.ru>
2023-05-13 23:01:58 +0300

    LAB5 Added

---
+++ LAB5/README.mm
@@ -0,0 +1,119 @@
+<map version="freeplane 1.11.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Обеспечение информационной безопасности в организации" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1684001165386" VGAP_QUANTITY="3 pt">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#eeeeee" zoom="0.769">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_grey_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_364653791" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#222222" BACKGROUND_COLOR="#eeeeee" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#676767" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_364653791" STARTINCLINATION="122.25 pt;0 pt;" ENDINCLINATION="119.25 pt;35.25 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#444444" WIDTH="3" DASH="SOLID"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#ffffff" BACKGROUND_COLOR="#3333ff" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#3333ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_1533685614" COLOR="#443333" BACKGROUND_COLOR="#dddddd" BORDER_WIDTH="3 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#cc0033" TRANSPARENCY="255" DESTINATION="ID_1533685614"/>
+<font SIZE="12" BOLD="true"/>
+<edge COLOR="#cc0033"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#000000" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffffff" BACKGROUND_COLOR="#222222" BORDER_COLOR="#2c2b29">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffffff" BACKGROUND_COLOR="#444444" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#ffffff" BACKGROUND_COLOR="#676767" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#ffffff" BACKGROUND_COLOR="#888888" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#222222" BACKGROUND_COLOR="#aaaaaa" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#222222" BACKGROUND_COLOR="#bbbbbb" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#222222" BACKGROUND_COLOR="#cccccc" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" COLOR="#222222" BACKGROUND_COLOR="#dddddd" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" COLOR="#222222" BACKGROUND_COLOR="#eeeeee" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" COLOR="#2c2b29" BACKGROUND_COLOR="#ffffff" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" COLOR="#222222" BACKGROUND_COLOR="#ffffff" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Общие положения" POSITION="bottom_or_right" ID="ID_1415612734" CREATED="1684001107543" MODIFIED="1684007783255" HGAP_QUANTITY="8 pt" VSHIFT_QUANTITY="141.75 pt" VGAP_QUANTITY="3 pt">
+<node TEXT="Цели и задачи документа" ID="ID_1970906591" CREATED="1684001238674" MODIFIED="1684007786368" HGAP_QUANTITY="43.25 pt" VSHIFT_QUANTITY="93 pt"/>
+<node TEXT="Область применения" ID="ID_568143483" CREATED="1684001315060" MODIFIED="1684007736686" HGAP_QUANTITY="43.25 pt" VSHIFT_QUANTITY="0 pt"/>
+<node TEXT="Список нормативных документов" ID="ID_1610520870" CREATED="1684001316268" MODIFIED="1684007771609" HGAP_QUANTITY="40.25 pt" VSHIFT_QUANTITY="-62.25 pt"/>
+</node>
+<node TEXT="Термины и определения" POSITION="top_or_left" ID="ID_858438925" CREATED="1684001448982" MODIFIED="1684007761975" HGAP_QUANTITY="-480.99999 pt" VSHIFT_QUANTITY="319.49999 pt" VGAP_QUANTITY="3 pt">
+<node TEXT="Основные термины и определения, используемые в документе" ID="ID_1840298748" CREATED="1684001494753" MODIFIED="1684007765347" HGAP_QUANTITY="-483.24999 pt" VSHIFT_QUANTITY="48.75 pt"/>
+</node>
+<node TEXT="Организационные меры по обеспечению информационной безопасности" POSITION="bottom_or_right" ID="ID_722263538" CREATED="1684001810318" MODIFIED="1684007794714" HGAP_QUANTITY="-0.25 pt" VSHIFT_QUANTITY="109.5 pt" VGAP_QUANTITY="3 pt">
+<node TEXT="Организация процесса обеспечения информационной безопасности" ID="ID_841006255" CREATED="1684001860429" MODIFIED="1684001899299" VSHIFT_QUANTITY="127.5 pt"/>
+<node TEXT="Руководство и управление информационной безопасностью" ID="ID_240601505" CREATED="1684001871851" MODIFIED="1684001913513"/>
+<node TEXT="Ответственность за нарушение информационной безопасности" ID="ID_889095768" CREATED="1684001881191" MODIFIED="1684001915633"/>
+</node>
+<node TEXT="Технические меры по обеспечению информационной безопасности" POSITION="top_or_left" ID="ID_515038787" CREATED="1684002054427" MODIFIED="1684007798978" HGAP_QUANTITY="-563.49998 pt" VSHIFT_QUANTITY="227.99999 pt" VGAP_QUANTITY="3 pt">
+<node TEXT="Информационная защита систем обработки информации" ID="ID_461306909" CREATED="1684002095844" MODIFIED="1684002156235" HGAP_QUANTITY="-585.24998 pt" VSHIFT_QUANTITY="135 pt"/>
+<node TEXT="Защита информации при передаче" ID="ID_45793930" CREATED="1684002097929" MODIFIED="1684002166784" HGAP_QUANTITY="-549.24998 pt"/>
+<node TEXT="Криптографические средства защиты информации" ID="ID_1610225165" CREATED="1684002099872" MODIFIED="1684002169726" HGAP_QUANTITY="-585.24998 pt"/>
+<node TEXT="Обеспечение контроля доступа к информации" ID="ID_1075198320" CREATED="1684002101733" MODIFIED="1684002175629" HGAP_QUANTITY="-583.74998 pt" VSHIFT_QUANTITY="-11.25 pt"/>
+</node>
+<node TEXT="Программные средства обеспечения информационной безопасности" POSITION="bottom_or_right" ID="ID_94126306" CREATED="1684007266494" MODIFIED="1684007808739" HGAP_QUANTITY="-2.5 pt" VSHIFT_QUANTITY="232.49999 pt" VGAP_QUANTITY="3 pt">
+<node TEXT="Программы для обнаружения и предотвращения атак на информационную систему" ID="ID_237201761" CREATED="1684007299392" MODIFIED="1684007322237" VSHIFT_QUANTITY="112.5 pt"/>
+<node TEXT="Антивирусные программы" ID="ID_133788297" CREATED="1684007302622" MODIFIED="1684007333900"/>
+<node TEXT="Средства защиты от несанкционированного доступа к информации" ID="ID_1132890957" CREATED="1684007304557" MODIFIED="1684007346331"/>
+</node>
+<node TEXT="Организация работы по обеспечению информационной безопасности" POSITION="top_or_left" ID="ID_1947290623" CREATED="1684007349615" MODIFIED="1684007804471" HGAP_QUANTITY="-563.49998 pt" VSHIFT_QUANTITY="230.24999 pt" VGAP_QUANTITY="3 pt">
+<node TEXT="Планирование работ по обеспечению информационной безопасности" ID="ID_1522941637" CREATED="1684007424578" MODIFIED="1684007471121" HGAP_QUANTITY="-577.74998 pt" VSHIFT_QUANTITY="124.5 pt"/>
+<node TEXT="Проведение аудита информационной безопасности" ID="ID_204814050" CREATED="1684007433709" MODIFIED="1684007474478" HGAP_QUANTITY="-579.24998 pt"/>
+<node TEXT="Обучение и подготовка сотрудников по вопросам информационной безопасности" ID="ID_838695212" CREATED="1684007445939" MODIFIED="1684007477132" HGAP_QUANTITY="-579.24998 pt"/>
+</node>
+<node TEXT="Контроль и мониторинг обеспечения информационной безопасности" POSITION="top_or_left" ID="ID_1510674842" CREATED="1684007481567" MODIFIED="1684007508552" HGAP_QUANTITY="-559.74998 pt" VGAP_QUANTITY="3 pt">
+<node TEXT="Методы контроля и мониторинга информационной безопасности" ID="ID_1072713142" CREATED="1684007504538" MODIFIED="1684007518476" HGAP_QUANTITY="-576.99998 pt" VSHIFT_QUANTITY="66.75 pt"/>
+<node TEXT="Оценка эффективности мер по обеспечению информационной безопасности" ID="ID_914988824" CREATED="1684007521107" MODIFIED="1684007548262" HGAP_QUANTITY="-575.49998 pt" VSHIFT_QUANTITY="6.75 pt"/>
+<node TEXT="Анализ и устранение уязвимостей в информационной системе" ID="ID_16620217" CREATED="1684007521310" MODIFIED="1684007551334" HGAP_QUANTITY="-574.74998 pt"/>
+</node>
+<node TEXT="Заключительные положения" POSITION="top_or_left" ID="ID_543700173" CREATED="1684007562355" MODIFIED="1684007588687" HGAP_QUANTITY="-506.49998 pt" VGAP_QUANTITY="3 pt">
+<node TEXT="Ответственность за выполнение требований документа" ID="ID_396264039" CREATED="1684007577262" MODIFIED="1684007609165" HGAP_QUANTITY="-570.99998 pt" VSHIFT_QUANTITY="72 pt"/>
+<node TEXT="Процедуры изменения и утверждения документа" ID="ID_610266576" CREATED="1684007585856" MODIFIED="1684007690051" HGAP_QUANTITY="-569.49998 pt" VSHIFT_QUANTITY="8.25 pt"/>
+</node>
+</node>
+</map>
